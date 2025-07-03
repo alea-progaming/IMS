88d76e5a3249be4d6e474443dace33d35ea3310b
Mikaela Purugganan <mikaela.programming@gmail.com>
2025-07-03 10:20:23 +0800

    ✅ Fetch data from database and display in HTML table
    🔜 Delete data from database

--- database/user-add.php
+++ database/user-add.php
@@ -8,7 +8,14 @@ session_start();
     $first_name = $_POST['fName'];
     $last_name = $_POST['lName'];
     $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
-    $password = $_POST['pass'];
+    
+    if(strlen($_POST['pass']) <= 8){
+      $_SESSION['shortPass'] = "Password should be more than 8 characters.";
+      header('location: ../users.php');
+      exit;
+    } else {
+      $password = $_POST['pass'];
+    }
     $encrypted = password_hash($password, PASSWORD_DEFAULT);
   }
   
@@ -18,7 +25,7 @@ session_start();
   // only execute the try block if the user entered a valid email
   if($email) {
     try {
-    $insert_user = "INSERT INTO $table_name(first_name, last_name, email, password) VALUES (:first_name, :last_name, :email, :encrypted)";
+    $insert_user = "INSERT INTO $table_name(first_name, last_name, email, password, created_at, updated_at) VALUES (:first_name, :last_name, :email, :encrypted, NOW(), NOW())";
     $stmt = $conn->prepare($insert_user);
     $stmt->execute([
       'first_name' => $first_name, 
@@ -27,7 +34,7 @@ session_start();
       'encrypted' => $encrypted]);
 
       $response = [
-        'success' => false,
+        'success' => true,
         'message' => $first_name . ' ' . $last_name . ' is successfully added to the system.'
       ];
     } catch (PDOException $e) {
--- users.php
+++ users.php
@@ -4,7 +4,9 @@
   if(!isset($_SESSION['user'])) header('Location: ../'); 
   $_SESSION['table'] = 'users'; // * turns out i actually forgot to put this here
   $user = $_SESSION['user'];
-
+  $users = include('database/show-users.php');
+    // var_dump($users);
+    // die;
 ?>
 
 <!DOCTYPE html>
@@ -23,15 +25,17 @@
     <div id="dashboardMainContainer">
       <!-- // * Sidebar -->
       <?php include('components/sidebar.php')?>
-    </div>
-    <div class="dashboardContentContainer" id="dashboardContentContainer">
+      </div>
+      <div class="dashboardContentContainer" id="dashboardContentContainer">
         <!-- // * Top Nav -->
         <?php include('components/topnav.php')?>
-        
         <div class="dashboardContent">
           <div class="contentMain">
-            <div class="userFormContainer">
-              <form action="database/user-add.php" method="POST" class="appForm">
+            <div class="row">
+              <div class="col col-5">
+                <h1 class="section_header"><i class="fa fa-plus"></i>Create User</h1>
+                <div class="userFormContainer">
+                  <form action="database/user-add.php" method="POST" class="appForm">
                 <div class="appFormDiv">
                   <label for="fName">First Name</label>
                   <input type="text" class="formInput" name="fName" id="fName">
@@ -50,25 +54,62 @@
                 </div>
                 <input type="hidden" name="table" value="users">
                 <button type="submit"><i class="fa fa-plus"></i>Add user</button>
-              </form>
-              <?php 
-                if (isset($_SESSION['invalidEmail'])) {
-                  // ! Fix invalid email style
-                  echo "<div><p class='responseMessage_error'>" . $_SESSION['invalidEmail'] . "</p></div>";
-                  unset($_SESSION['invalidEmail']); // So it doesn’t stick forever
-                }
+                </form>
+                  <?php 
+                    if(isset($_SESSION['shortPass'])) {
+                      echo "<div><p class='responseMessage_error'>" . $_SESSION['shortPass'] . "</p></div>";
+                      unset($_SESSION['shortPass']); // So it doesn’t stick forever
+                    }
 
-                if(isset($_SESSION['response'])) {
-                  $response_msg = $_SESSION['response']['message'];
-                  $is_success = $_SESSION['response']['success' ];
-              ?>
-              <div class="responseMessage">
-                <p class="<?= $is_success ? 'responseMessage_success' : 'responseMessage_error' ?>">
-                  <?= $response_msg ?>  
-                </p>
+                    if (isset($_SESSION['invalidEmail'])) {
+                      echo "<div><p class='responseMessage_error'>" . $_SESSION['invalidEmail'] . "</p></div>";
+                      unset($_SESSION['invalidEmail']); // So it doesn’t stick forever
+                    }
+
+                    if(isset($_SESSION['response'])) {
+                      $response_msg = $_SESSION['response']['message'];
+                      $is_success = $_SESSION['response']['success' ];
+                  ?>
+                  <div class="responseMessage">
+                    <p class="<?= $is_success ? 'responseMessage_success' : 'responseMessage_error' ?>">
+                      <?= $response_msg ?>  
+                    </p>
+                  </div>
+                  <?php unset($_SESSION['response']); } ?>
+                </div>
               </div>
-              <?php unset($_SESSION['response']); } ?>
+              <div class="col col-7">
+                <h1 class="section_header"><i class="fa fa-list"></i>List of Users</h1>
+                <div class="section_content">
+                  <div class="users">
+                    <table>
+                      <tr>
+                        <th>#</th>
+                        <th>Last Name</th>
+                        <th>First Name</th>
+                        <th>Email</th>
+                        <th>Created at</th>
+                        <th>Updated at</th>
+                      </tr>
+                      <tbody>
+                        <?php foreach($users as $index => $user) { ?>
+                        <tr>
+                          <td><?= $index + 1 ?></td>
+                          <td><?= $user['last_name'] ?></td>
+                          <td><?= $user['first_name'] ?></td>
+                          <td><?= $user['email'] ?></td>
+                          <td><?= date('M d, Y @ H:i:s', strtotime($user['created_at'])); ?></td>
+                          <td><?= date('M d, Y @ H:i:s', strtotime($user['updated_at'])); ?></td>
+                        </tr>
+                        <?php } ?>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
             </div>
+            
+            
           </div>
         </div>
       </div>
